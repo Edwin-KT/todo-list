7bddfac5903123c60f06697d0b9dd3d3a49e0546
Edwin <77319863+Edwin-KT@users.noreply.github.com>
2025-07-13 13:07:13 +0300

    style project section

--- src/display/projectDisplayer.js
+++ src/display/projectDisplayer.js
@@ -1,38 +1,47 @@
+import delTaskIcon from "../media/icons/task-div/delete-task-icon.svg";
+
 export default function displayProject(project) {
   const projectContainer = document.getElementById("selected-project");
-
   projectContainer.innerHTML = "";
 
   const title = document.createElement("p");
   title.textContent = `${project.name}`;
+  title.id = "project-title";
 
   const addNewTaskBtn = document.createElement("button");
   addNewTaskBtn.textContent = "Add New Task";
+  addNewTaskBtn.id = "add-new-task-btn";
 
   const tasksContainer = document.createElement("div");
+  tasksContainer.id = "tasks-container";
   const tasksToDoTitle = document.createElement("p");
   tasksToDoTitle.textContent = `Tasks to do - ${project.tasks.size}`;
+  tasksToDoTitle.id = "tasks-to-do-title";
   tasksContainer.append(tasksToDoTitle);
   project.tasks.forEach((task) => {
     const taskDiv = document.createElement("div");
     const taskTitle = document.createElement("p");
     taskTitle.textContent = `${task.title}`;
     taskDiv.dataset.id = task.id;
+    taskDiv.classList.add("task-div");
 
     taskDiv.append(taskTitle);
 
     const markDoneBtn = document.createElement("button");
     markDoneBtn.innerHTML = "&#10003;";
-    markDoneBtn.addEventListener("click", () => {});
+    markDoneBtn.classList.add("mark-done-btn");
 
     const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener("click", function () {
-      project.removeTask(task.id);
-      displayProject(project);
-    });
+    deleteBtn.classList.add("delete-task-btn");
+    const deleteIcon = new Image();
+    deleteIcon.src = delTaskIcon;
+    deleteIcon.alt = "Delete";
 
-    taskDiv.append(markDoneBtn, deleteBtn);
+    deleteBtn.appendChild(deleteIcon);
+
+    const buttonsContainer = document.createElement("div");
+    buttonsContainer.append(markDoneBtn, deleteBtn);
+    taskDiv.append(buttonsContainer);
 
     tasksContainer.append(taskDiv);
   });
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+
+import displayProject from "./display/ProjectDisplayer";
+import Task from "./modules/Task";
+import Project from "./modules/Project";
+import TodoList from "./modules/TodoList";
+
+import TodoController from "./controllers/TodoController";
+
+const myProject = new Project("My Project");
+
+const task1 = new Task("Curat in camera", "");
+const task2 = new Task("Spalat vase", "");
+const task3 = new Task("Tuns iarba", "");
+
+const TodoApp = new TodoController();
+
+TodoApp.addProject(myProject);
+TodoApp.addTask(task1);
+TodoApp.addTask(task2);
+TodoApp.addTask(task3);
+
+// // todolist controller leaga index cu ui de ex cand adaug un project trb sa il adaug in todolist si trb sa updatez si pagina
+
+const anotherProject = new Project("Birou");
+
+const anotherTask = new Task("codeaza", "");
+
+anotherProject.addTask(anotherTask);
+
+TodoApp.addProject(anotherProject);
+TodoApp.addTask(anotherTask);
+
+TodoApp.switchToProject(myProject);
